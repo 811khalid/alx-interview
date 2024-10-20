2d760f36d389a88a0556f1dd0fd3a8f1694557e1
811khalid <khalidtewfiq151@gmail.com>
2024-10-20 00:12:09 -0700

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-
+"""
+Module for calculating the minimum number of operations to achieve n H characters.
+"""
 
 def minOperations(n):
+    """Calculates the minimum number of operations to achieve n H characters."""
     if n <= 1:
         return 0
-    
+
     operations = 0
-    factor = 2
-    
+    divisor = 2
+
+    # Prime factorization approach to find the minimum number of operations
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    
+        while n % divisor == 0:
+            operations += divisor
+            n /= divisor
+        divisor += 1
+
     return operations
+
