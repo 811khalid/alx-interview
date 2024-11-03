137996c8d1bdb4a932500c229f1bbe81d0c617d8
811khalid <khalidtewfiq151@gmail.com>
2024-11-03 16:44:04 +0000

    fix6

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,36 +3,33 @@
 Module for validating UTF-8 encoding
 """
 
+
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
-    
+
     Parameters:
     data (List[int]): List of integers representing bytes of data.
-    
+
     Returns:
     bool: True if data is a valid UTF-8 encoding, else False.
     """
-    # Number of bytes remaining in the current UTF-8 character
-    num_bytes = 0
-    
-    # Masks to check the start byte and continuation bytes
+    num_bytes = 0  # Number of bytes remaining in the current UTF-8 character
     mask1 = 1 << 7  # 10000000
     mask2 = 1 << 6  # 01000000
 
     for byte in data:
-        # Only consider the least significant 8 bits
-        byte = byte & 0xFF
+        byte = byte & 0xFF  # Consider only the least significant 8 bits
 
         if num_bytes == 0:
             # Determine the number of bytes in the UTF-8 character
-            if (byte >> 5) == 0b110:  # 2-byte character
+            if (byte >> 5) == 0b110:       # 2-byte character
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:  # 3-byte character
+            elif (byte >> 4) == 0b1110:    # 3-byte character
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 4-byte character
+            elif (byte >> 3) == 0b11110:   # 4-byte character
                 num_bytes = 3
-            elif (byte >> 7):  # Invalid 1-byte character (should be 0xxxxxxx)
+            elif (byte >> 7):              # Invalid 1-byte character
                 return False
         else:
             # Check if it's a valid continuation byte (10xxxxxx)
@@ -40,5 +37,4 @@ def validUTF8(data):
                 return False
             num_bytes -= 1
 
-    # All characters should be complete (num_bytes should be 0)
-    return num_bytes == 0
+    return num_bytes == 0  # All characters should be complete
